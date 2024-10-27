92d7d696131ef7c0cf826e2b8c218e4bba65199a
Akshay Gaur <akshay.gaur028@gmail.com>
2024-10-27 14:31:43 +0530

    footer fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,11 +10,13 @@ const App = () => {
   return (
     <div className="min-h-screen bg-slate-50">
       <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/result" element={<Result/>} />
-        <Route path="/buy" element={<BuyCredit/>} />
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/result" element={<Result/>} />
+          <Route path="/buy" element={<BuyCredit/>} />
+        </Routes>
+      </div>
       <Footer/>
     </div>
   )
